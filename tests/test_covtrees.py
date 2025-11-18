944f87f2afd7a9f70287e38dae3e1f3a37b3c907
Vinzenz May <vinzenz.may@bih-charite.de>
2025-11-18 15:55:43 +0100

    fixed formatting

--- tests/test_covtrees.py
+++ tests/test_covtrees.py
@@ -1,12 +1,11 @@
-#%%
-from pathlib import Path
+# %%
 
 import numpy as np
 from intervaltree import Interval, IntervalTree
 
 from svirlpool.util import covtree
 
-#%%
+# %%
 
 # ================================================================
 # ----------100           -> 1,0,100,r0
@@ -15,20 +14,50 @@ from svirlpool.util import covtree
 # -> (0,20,1), (20,80,2), (80,100,3), (100,120,2), (120,180,1)
 # ================================================================
 
+
 def test_parallel_coverage_computation__simple():
-    all_positions = {'1': [0, 20, 80, 100, 120, 180]}
-    intervall_trees = {'1': IntervalTree([Interval(0, 100, 'r0'), Interval(20, 120, 'r1'), Interval(80, 180, 'r2')])}
-    num_workers=2
-    result = covtree.parallel_coverage_computation(all_positions=all_positions, intervall_trees=intervall_trees, num_workers=num_workers)
-    expected = {'1': IntervalTree([Interval(0, 20, 1), Interval(20, 80, 2), Interval(80, 100, 3), Interval(100, 120, 2), Interval(120, 180, 1)])}
+    all_positions = {"1": [0, 20, 80, 100, 120, 180]}
+    intervall_trees = {
+        "1": IntervalTree([
+            Interval(0, 100, "r0"),
+            Interval(20, 120, "r1"),
+            Interval(80, 180, "r2"),
+        ])
+    }
+    num_workers = 2
+    result = covtree.parallel_coverage_computation(
+        all_positions=all_positions,
+        intervall_trees=intervall_trees,
+        num_workers=num_workers,
+    )
+    expected = {
+        "1": IntervalTree([
+            Interval(0, 20, 1),
+            Interval(20, 80, 2),
+            Interval(80, 100, 3),
+            Interval(100, 120, 2),
+            Interval(120, 180, 1),
+        ])
+    }
     assert result == expected
-    
+
+
 def test_construct_interval_trees_simple():
     data = np.array([
-        ['1', '0', '100', 'r0'],
-        ['1', '20', '120', 'r1'],
-        ['1', '80', '180', 'r2']])
+        ["1", "0", "100", "r0"],
+        ["1", "20", "120", "r1"],
+        ["1", "80", "180", "r2"],
+    ])
     result = covtree.construct_interval_trees(data=data)
-    expected = {'1': IntervalTree([Interval(0, 100, 'r0'), Interval(20, 120, 'r1'), Interval(80, 180, 'r2')])}, {'1': [0, 20, 80, 100, 120, 180]}
+    expected = (
+        {
+            "1": IntervalTree([
+                Interval(0, 100, "r0"),
+                Interval(20, 120, "r1"),
+                Interval(80, 180, "r2"),
+            ])
+        },
+        {"1": [0, 20, 80, 100, 120, 180]},
+    )
     assert result[0] == expected[0]
     assert result[1] == expected[1]
