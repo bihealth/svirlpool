55c61df5e0e5f11838f9589582c53fc23c52f32b
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2025-12-20 23:07:11 +0100

    feat: add Docker builds (#37) (#38)
    Closes: #37

---
+++ utils/docker/build-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Utility script to start the Docker build process.
+
+set -x
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+GIT_DESCRIBE=$(git describe --tags | cut -d - -f 1)
+GIT_TAG=${GIT_TAG-$GIT_DESCRIBE}
+DOCKER_VERSION=$(echo $GIT_TAG | sed -e 's/^v//')
+
+ORG=bihealth
+REPO=svirlpool
+
+docker build . \
+    --file utils/docker/Dockerfile \
+    --pull \
+    -t ghcr.io/$ORG/$REPO:$DOCKER_VERSION \
+    "$@"
